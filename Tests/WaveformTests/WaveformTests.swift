03feffdf3aab4a1ee57441f3eacf344ac7f734d1
Taylor Holliday <wtholliday@gmail.com>
2022-09-23 18:47:47 -0700

    Update WaveformTests.swift

--- Tests/WaveformTests/WaveformTests.swift
+++ Tests/WaveformTests/WaveformTests.swift
@@ -1,9 +1,22 @@
 import XCTest
 @testable import Waveform
+import AVFoundation
 
 final class WaveformTests: XCTestCase {
     func testRenderWaveform() throws {
-        let url = Bundle.module.url(forResource: "beat", withExtension: "aiff")
-        XCTAssertNotNil(url)
+        guard let url = Bundle.module.url(forResource: "beat", withExtension: "aiff") else {
+            XCTFail()
+            return
+        }
+        
+        let file = try! AVAudioFile(forReading: url)
+        
+        let audioFormat = AVAudioFormat(commonFormat: .pcmFormatFloat32,
+                                        sampleRate: file.fileFormat.sampleRate,
+                                        channels: file.fileFormat.channelCount, interleaved: false)!
+        
+        let buffer = AVAudioPCMBuffer(pcmFormat: audioFormat, frameCapacity: AVAudioFrameCount(file.length))!
+        
+        try! file.read(into: buffer)
     }
 }
