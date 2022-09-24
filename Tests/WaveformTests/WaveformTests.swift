1ffa79f43ef88589ccb568cc1dc06db72329aaa1
Taylor Holliday <wtholliday@gmail.com>
2022-09-23 17:57:43 -0700

    swift package init

---
+++ Tests/WaveformTests/WaveformTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Waveform
+
+final class WaveformTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Waveform().text, "Hello, World!")
+    }
+}
