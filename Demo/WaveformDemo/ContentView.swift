af241f7ec0c660de3fbd7536901fc7c9824eca68
Aurelius Prochazka <aurephd@gmail.com>
2022-09-29 15:44:03 -0700

    samples added to demo

--- Demo/WaveformDemo/ContentView.swift
+++ Demo/WaveformDemo/ContentView.swift
@@ -2,9 +2,19 @@ import SwiftUI
 import Waveform
 
 struct ContentView: View {
+
+    var samples: [Float] {
+        var s: [Float] = []
+        let size = 128
+        for i in 0..<size {
+            let sine = sin(Float(i * 2) * .pi / Float(size))
+            s.append(sine + 0.1 * Float.random(in: 0...1))
+        }
+        return s
+    }
     var body: some View {
         VStack {
-            Waveform(constants: Constants())
+            Waveform(samples: samples, constants: Constants())
         }
         .padding()
     }
