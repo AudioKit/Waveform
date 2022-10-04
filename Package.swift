3a129cc1b3199bd133d7cd8253a9f492c23eaebc
Aurelius Prochazka <aurephd@gmail.com>
2022-10-03 23:11:57 -0700

    Added docc file as a resource

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     platforms: [.macOS(.v11), .iOS(.v14)],
     products: [.library(name: "Waveform", targets: ["Waveform"])],
     targets: [
-        .target(name: "Waveform", dependencies: []),
+        .target(name: "Waveform", resources: [.process("Waveform.docc")]),
         .testTarget(name: "WaveformTests", dependencies: ["Waveform"], resources: [.copy("beat.aiff")]),
     ]
 )
