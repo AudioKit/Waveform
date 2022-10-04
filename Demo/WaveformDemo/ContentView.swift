061bcc3da8bb30d7ab1891328cd8a2b353f52ed4
Aurelius Prochazka <aurephd@gmail.com>
2022-10-03 21:46:49 -0700

    Moved minimap to its own file

--- Demo/WaveformDemo/ContentView.swift
+++ Demo/WaveformDemo/ContentView.swift
@@ -22,55 +22,6 @@ func clamp(_ x: Double, _ inf: Double, _ sup: Double) -> Double {
     max(min(x, sup), inf)
 }
 
-struct MinimapView: View {
-    @Binding var start: Double
-    @Binding var length: Double
-
-    @GestureState var initialStart: Double?
-    @GestureState var initialLength: Double?
-
-    let indicatorSize = 10.0
-
-    var body: some View {
-        GeometryReader { gp in
-            RoundedRectangle(cornerRadius: indicatorSize)
-                .frame(width: length * gp.size.width)
-                .offset(x: start * gp.size.width)
-                .opacity(0.5)
-                .gesture(DragGesture()
-                    .updating($initialStart) { _, state, _ in
-                        if state == nil {
-                            state = start
-                        }
-                    }
-                    .onChanged { drag in
-                        if let initialStart = initialStart {
-                            start = clamp(initialStart + drag.translation.width / gp.size.width, 0, 1 - length)
-                        }
-                    }
-                )
-
-            RoundedRectangle(cornerRadius: indicatorSize)
-                .foregroundColor(.white)
-                .frame(width: indicatorSize).opacity(0.3)
-                .offset(x: (start + length) * gp.size.width)
-                .padding(indicatorSize)
-                .gesture(DragGesture()
-                    .updating($initialLength) { _, state, _ in
-                        if state == nil {
-                            state = length
-                        }
-                    }
-                    .onChanged { drag in
-                        if let initialLength = initialLength {
-                            length = clamp(initialLength + drag.translation.width / gp.size.width, 0, 1 - start)
-                        }
-                    }
-                )
-        }
-    }
-}
-
 struct ContentView: View {
     @StateObject var model = WaveformDemoModel(file: getFile())
 
@@ -92,11 +43,5 @@ struct ContentView: View {
             .foregroundColor(.blue)
         }
         .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
---
+++ Demo/WaveformDemo/MinimapView.swift
@@ -0,0 +1,54 @@
+// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/Waveform/
+
+import AVFoundation
+import SwiftUI
+import Waveform
+
+struct MinimapView: View {
+    @Binding var start: Double
+    @Binding var length: Double
+
+    @GestureState var initialStart: Double?
+    @GestureState var initialLength: Double?
+
+    let indicatorSize = 10.0
+
+    var body: some View {
+        GeometryReader { gp in
+            RoundedRectangle(cornerRadius: indicatorSize)
+                .frame(width: length * gp.size.width)
+                .offset(x: start * gp.size.width)
+                .opacity(0.2)
+                .gesture(DragGesture()
+                    .updating($initialStart) { _, state, _ in
+                        if state == nil {
+                            state = start
+                        }
+                    }
+                    .onChanged { drag in
+                        if let initialStart = initialStart {
+                            start = clamp(initialStart + drag.translation.width / gp.size.width, 0, 1 - length)
+                        }
+                    }
+                )
+
+            RoundedRectangle(cornerRadius: indicatorSize)
+                .foregroundColor(.white)
+                .frame(width: indicatorSize).opacity(0.2)
+                .offset(x: (start + length) * gp.size.width)
+                .padding(indicatorSize)
+                .gesture(DragGesture()
+                    .updating($initialLength) { _, state, _ in
+                        if state == nil {
+                            state = length
+                        }
+                    }
+                    .onChanged { drag in
+                        if let initialLength = initialLength {
+                            length = clamp(initialLength + drag.translation.width / gp.size.width, 0, 1 - start)
+                        }
+                    }
+                )
+        }
+    }
+}
